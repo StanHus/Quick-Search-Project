216e579bc4d8273c9c779bcd79b9f16bb8f59aa1
StanHus <86333618+StanHus@users.noreply.github.com>
2022-07-22 23:46:40 +0100

    changed colours and changed landing page pp link

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -4,9 +4,9 @@ export default function LandingPage() {
   return (
     <Fragment>
       <h2>
-        I wasn not sure what to put here, sooo.. here is a link to my (Latest){' '}
+        I wasn not sure what to put here, sooo.. here is{' '}
         <a className="external-link" href="https://kid-360.netlify.app/">
-          Personal Project
+          a link to my (Latest) Personal Project
         </a>
       </h2>
       <h2>Check out Parent Mode with Master password: firebase</h2>
